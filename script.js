03e8a6defb1eb3c7e252ef94d77074d479a04e66
Parvathy A <145282805+parvathyamvk@users.noreply.github.com>
2024-08-21 23:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
